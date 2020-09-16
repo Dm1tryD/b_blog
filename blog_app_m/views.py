70d20a01dd3b6b64107f979e8522e5dfaae09da7
Dm1tryD <dmitrydanyliukk@gmail.com>
2020-09-16 14:34:26 +0300

    add html form for posts

--- blog_app_m/views.py
+++ blog_app_m/views.py
@@ -3,3 +3,8 @@ from .models import Post
 def posts_list(request):
     posts = Post.objects.all()
     return render(request,'blog_app_m/index.html', context={'posts':posts})
+
+
+def post_detail(request,slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog_app_m/post_detail.html',context={'post':post})
