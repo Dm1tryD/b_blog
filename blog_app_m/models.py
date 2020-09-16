6efc50df426e6dcf5aaa4af6fc04b249e0630657
Dm1tryD <dmitrydanyliukk@gmail.com>
2020-09-16 18:42:52 +0300

    add tags and mixins as utils

--- blog_app_m/models.py
+++ blog_app_m/models.py
@@ -1,12 +1,28 @@
 from django.db import models
+from django.shortcuts import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateField(auto_now_add=True)
     date_change = models.DateField(auto_now=True)
 
+    def get_absolute_url(self):
+        """Returns the generated url link"""
+        return reverse('post_detail_url',kwargs={'slug':self.slug})
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50,unique=True)
+
+    def get_absolute_url(self):
+        """Returns the generated url link"""
+        return reverse('tag_detail_url',kwargs={'slug':self.slug})
 
     def __str__(self):
         return '{}'.format(self.title)
--- blog_app_m/views.py
+++ blog_app_m/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-from .models import Post
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
+
+from .models import Post,Tag
+
 def posts_list(request):
     posts = Post.objects.all()
     return render(request,'blog_app_m/index.html', context={'posts':posts})
 
+class PostDetail(View):
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug__iexact=slug)
+        return render(request, 'blog_app_m/post_detail.html', context={'post': post})
 
-def post_detail(request,slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog_app_m/post_detail.html',context={'post':post})
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog_app_m/tags_list.html', context={'tags':tags})
+
+class TagDetail(View):
+    def get(self, request, slug):
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        return render(request, 'blog_app_m/tag_detail.html', context={'tag': tag})
